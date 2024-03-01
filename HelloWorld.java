b9fa6fba4c5dcf587060aca9bf211cecc0a249c0
m4theuzzz <mat.vsantos@outlook.com>
2024-03-01 20:06:20 -0300

    feat: trivial

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Engenharia Econômica para software");
+        System.out.println("Matheus Vieira dos Santos");
+        // testes
+        // testes
+        // testes
+        // testes
+        // testes
+        // testes
+        // testes
+    }
+}
